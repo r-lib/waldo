77816c65ae13bb9b0b7ab538d89041b0db01cfa0
Hadley Wickham <h.wickham@gmail.com>
2020-03-28 19:49:14 -0500

    Test multiple contexts

--- tests/testthat/test-compare-value.R
+++ tests/testthat/test-compare-value.R
@@ -8,6 +8,12 @@ test_that("multiplication works", {
     compare_value(c("a", "b"), c("a", "b", "c"))
     compare_value(c("a", "B", "c"), c("a", "b", "c"))
 
+    "multiple contexts"
+    compare_value(
+      c("a", "b", letters, "a", "b", "c", letters, "X"),
+      c("a", "b", "c", letters, "a", "b", letters, "Y")
+    )
+
     "truncation"
     compare_value(c("X", letters), letters)
     compare_value(c(letters, "X"), letters)
