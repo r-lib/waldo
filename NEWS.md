ea546247a59d8408f4ed0c12cf54bc6cb7d037d5
Hadley Wickham <h.wickham@gmail.com>
2020-07-08 13:12:20 -0500

    Export compare_proxy()
    Fixes #22

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,9 @@
 # waldo (development version)
 
+* `compare_proxy()` is now exported so that you can provide methods if your
+  objects need special handling (particularly needed for objects that contain
+  external pointers) (#22).
+
 * A trailing newline is no longer ignored when comparing strings (#37).
 
 # waldo 0.1.0
--- R/proxy.R
+++ R/proxy.R
@@ -1,3 +1,22 @@
+#' Proxy for waldo comparison
+#'
+#' @description
+#' Use this generic to override waldo's default comparison if you need to
+#' override the defaults (typically because your object contains an external
+#' pointer).
+#'
+#' waldo comes with methods for two common cases:
+#'
+#' * data.table: the `.internal.selfref` attribute is set to `NULL`. This is
+#'   an external pointer that is used for performance optimisation, and
+#'   doesn't affect the data.
+#'
+#' * `xml2::xml_node`: the underlying XML data is stored in memory in C,
+#'   behind an external pointer, so the we best can do is to convert the
+#'   object to a string.
+#'
+#' @param x An object.
+#' @export
 compare_proxy <- function(x) {
   UseMethod("compare_proxy")
 }
