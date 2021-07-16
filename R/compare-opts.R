46e3525824d1237a0f0b792e32767a41f48cd3f6
Hadley Wickham <h.wickham@gmail.com>
2021-07-16 13:48:18 -0500

    Match argument order in compare

--- R/compare-opts.R
+++ R/compare-opts.R
@@ -4,8 +4,8 @@ compare_opts <- function(...,
                          ignore_srcref = TRUE,
                          ignore_attr = FALSE,
                          ignore_encoding = TRUE,
-                         ignore_formula_env = FALSE,
-                         ignore_function_env = FALSE
+                         ignore_function_env = FALSE,
+                         ignore_formula_env = FALSE
                          ) {
 
   base <- old_opts(...)
@@ -16,8 +16,8 @@ compare_opts <- function(...,
     ignore_srcref = ignore_srcref,
     ignore_attr = ignore_attr,
     ignore_encoding = ignore_encoding,
-    ignore_formula_env = ignore_formula_env,
-    ignore_function_env = ignore_function_env
+    ignore_function_env = ignore_function_env,
+    ignore_formula_env = ignore_formula_env
   )
 
   utils::modifyList(waldo, base)
