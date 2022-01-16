4c6bed731ea1265672049659e0f0c8a1524e8b4d
Michael Chirico <michaelchirico4@gmail.com>
2022-01-16 15:15:43 -0500

    Fix RProtoBuf proxy comparison (#120)
    Closes #119

--- tests/testthat/test-proxy.R
+++ tests/testthat/test-proxy.R
@@ -8,3 +8,25 @@ test_that("xml2 proxy generates useful comparisons", {
     compare(x1a, x2)
   })
 })
+
+# don't Suggest RProtoBuf, so just mock the classes
+test_that("RProtoBuf proxy works", {
+  x1 <- x2 <- list(toString = function(x) 1)
+  # strengthen confidence by ensuring toString() is compared
+  x1$a <- 2
+  x2$a <- 3
+  proto_methods <-  c(
+    "Message",
+    "Descriptor",
+    "EnumDescriptor",
+    "FieldDescriptor",
+    "ServiceDescriptor",
+    "FileDescriptor",
+    "EnumValueDescriptor",
+    "MethodDescriptor"
+  )
+  for (method in proto_methods) {
+    class(x1) <- class(x2) <- method
+    expect_identical(x1, x2)
+  }
+})
